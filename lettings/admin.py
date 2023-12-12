a8e05f19b17a0dc3bdca49940b6b610289440287
EL-WALID EL-KHABOU <alkhabou@gmail.com>
2023-12-12 14:35:52 +0100

    Création de mon répo + Création des deux applications (lettings et profiles), puis changement des models des deux application et ce depuis le main models.py de l'ancienne version, ainsi que les deux fichiers admin.py

---
+++ lettings/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Address, Letting
+
+admin.site.register(Address)
+admin.site.register(Letting)
---
+++ oc_lettings_site/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from lettings.models import Letting  # Importez Letting depuis lettings.models
+from profiles.models import Profile  # Importez Profile depuis profiles.models
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def lettings_index(request):
+    lettings_list = Letting.objects.all()
+    context = {"lettings_list": lettings_list}
+    return render(request, "lettings_index.html", context)
+
+
+def letting(request, letting_id):
+    letting = Letting.objects.get(id=letting_id)
+    context = {
+        "title": letting.title,
+        "address": letting.address,
+    }
+    return render(request, "letting.html", context)
+
+
+def profiles_index(request):
+    profiles_list = Profile.objects.all()
+    context = {"profiles_list": profiles_list}
+    return render(request, "profiles_index.html", context)
+
+
+def profile(request, username):
+    profile = Profile.objects.get(user__username=username)
+    context = {"profile": profile}
+    return render(request, "profile.html", context)
