316ec9686ab111e15b253aae5193bbf26b570761
EL-WALID EL-KHABOU <alkhabou@gmail.com>
2023-12-14 01:06:50 +0100

    Suppression des anciennes tables en manipulant les fichier de migration, et transfert des templates, chacun dans sa propre application

--- lettings/views.py
+++ lettings/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Letting
 
-# Create your views here.
+
+def lettings_index(request):
+    lettings_list = Letting.objects.all()
+    context = {"lettings_list": lettings_list}
+    return render(request, "lettings/lettings_index.html", context)
+
+
+def letting(request, letting_id):
+    letting = Letting.objects.get(id=letting_id)
+    context = {
+        "title": letting.title,
+        "address": letting.address,
+    }
+    return render(request, "lettings/letting.html", context)
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
+
+def profiles_index(request):
+    profiles_list = Profile.objects.all()
+    context = {"profiles_list": profiles_list}
+    return render(request, "profiles/profiles_index.html", context)
+
+
+def profile(request, username):
+    profile = Profile.objects.get(user__username=username)
+    context = {"profile": profile}
+    return render(request, "profiles/profile.html", context)
