d263b1cbedeca4b4e8409e8e87f0c9fb5fd10ebe
EL-WALID EL-KHABOU <alkhabou@gmail.com>
2023-12-14 02:57:06 +0100

    Création des espaces de nom pour les URL des deux nouvelles applications et vérification

--- lettings/urls.py
+++ lettings/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "lettings"
+
 urlpatterns = [
     path("", views.lettings_index, name="lettings_index"),
     path("<int:letting_id>/", views.letting, name="letting"),
--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "profiles"
+
 urlpatterns = [
     path("", views.profiles_index, name="profiles_index"),
     path("<str:username>/", views.profile, name="profile"),
