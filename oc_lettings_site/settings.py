b8110cdba3c849a954724e6ce1a8720957095b89
EL-WALID EL-KHABOU <alkhabou@gmail.com>
2024-01-17 10:36:54 +0100

    Ajout du Procfile pour le déploiement et ajustement du settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "votre_url_render.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "oc-p13-a8c2.onrender.com"]
 
 
 # Application definition
